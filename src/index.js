c9a0af02ed9c04912df7ad7591333dfda65c827a
markyong <markyong14@gmail.com>
2021-01-24 14:36:52 +0800

    refactor: adjust

--- src/index.js
+++ src/index.js
@@ -24,32 +24,35 @@ function setProp(component, prop, propValue) {
 }
 
 function vueSetProps(Vue, opts) {
-  if (isPlainObject(opts)) {
-    const { components } = Vue.options
-    Object.keys(opts).forEach(name => {
-      const component = components[name]
-      if (component) {
-        const setProps = opts[name]
-        if (process.env.NODE_ENV !== 'production' && !isPlainObject(setProps)) {
-          warn(`The ${name} component set props options expected an Object, ` +
-          `but got ${toRawType(setProps)}.`)
-          return
-        }
-
-        Object.keys(setProps).forEach(prop => {
-          setProp(component.options, prop, setProps[prop])
-          if (process.env.NODE_ENV !== 'production' && !hasProp) {
-            warn(`Can not find prop "${prop}" in the ${name} component.`)
-          }
-          hasProp = false
-        })
-      } else if (process.env.NODE_ENV !== 'production') {
-        warn(`Can not find ${name} component in global components.`)
-      }
-    })
-  } else if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV !== 'production' && !isPlainObject(opts)) {
     warn(`The options expected an Object, but got ${toRawType(opts)}.`)
+    return
   }
+
+  const { components } = Vue.options
+  Object.keys(opts).forEach(name => {
+    const component = components[name]
+    if (component) {
+      const setProps = opts[name]
+      if (process.env.NODE_ENV !== 'production' && !isPlainObject(setProps)) {
+        warn(
+          `The ${name} component set props options expected an Object, ` +
+          `but got ${toRawType(setProps)}.`
+        )
+        return
+      }
+
+      Object.keys(setProps).forEach(prop => {
+        setProp(component.options, prop, setProps[prop])
+        if (process.env.NODE_ENV !== 'production' && !hasProp) {
+          warn(`Can not find prop "${prop}" in the ${name} component.`)
+        }
+        hasProp = false
+      })
+    } else if (process.env.NODE_ENV !== 'production') {
+      warn(`Can not find ${name} component in global components.`)
+    }
+  })
 }
 
 export default vueSetProps
